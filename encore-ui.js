097f6aaa5b8aab6ff27a5401e92ce8b22dff2218
Ryan A. Johnson <ryan.johnson@rackspace.com>
2017-04-26 16:06:19 -0500

    chore(bower): release v4.1.0

--- encore-ui.js
+++ encore-ui.js
@@ -2,7 +2,7 @@
  * EncoreUI
  * https://github.com/rackerlabs/encore-ui
  *
- * Version: 4.1.0-0 - 2017-04-26
+ * Version: 4.1.0 - 2017-04-26
  * License: Apache-2.0
  */
 angular.module('encore.ui', [
